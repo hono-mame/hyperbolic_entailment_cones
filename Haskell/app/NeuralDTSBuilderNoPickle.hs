55cc718eab8fa7c6528f692577cd155af981a5da
Honoka Kobayashi <hononyanpo@gmail.com>
2025-11-25 21:34:05 +0900

    feat: NeuralDTSBuilderの実装(具体的なロジックの実装と、Poincare BallsのEmbeddingを直接読み込むテスト関数の作成)

--- Haskell/app/NeuralDTSBuilderNoPickle.hs
+++ Haskell/app/NeuralDTSBuilderNoPickle.hs
@@ -9,13 +9,7 @@ import qualified Data.Text.Read as TR
 import System.IO
 import Data.Maybe (fromMaybe, mapMaybe)
 import Data.List (foldl')
-
-
-angleChild :: [Float] -> [Float] -> Float
-angleChild parentEmb childEmb = 0.0
-
-angleParent :: [Float] -> [Float] -> Float -> Float
-angleParent parentEmb childEmb alpha = 3.0 * alpha
+import NeuralDTSBuilderUtils (angleChild, angleParent)
 
 loadVocab :: FilePath -> IO (M.Map String Int)
 loadVocab path = do
@@ -54,8 +48,8 @@ loadEmbeddings path = do
     pure $ M.fromList (mapMaybe parseLine ls)
 
 
-neuralDTSBuilderNoPickle :: IO (String -> String -> Float)
-neuralDTSBuilderNoPickle = do
+neuralDTSBuilder :: IO (String -> String -> Float)
+neuralDTSBuilder = do
     let embPath   = "data/task-0percent_dim-5_class-HypCones_init_class-PoincareNIPS_neg_sampl_strategy-true_neg_lr-0.0003_epochs-300_opt-rsgd_where_not_to_sample-children_neg_edges_attach-parent_lr_init-0.03_epochs_init-100_n_word_vectors.csv"
     let vocabPath = "data/jp_nouns_head_10000_closure.tsv.vocab"
     let alpha     = 0.1
@@ -73,10 +67,11 @@ neuralDTSBuilderNoPickle = do
                 (Just pIdx, Just cIdx) ->
                     case (M.lookup pIdx embMap, M.lookup cIdx embMap) of
                         (Just pEmb, Just cEmb) ->
-                            let a1 = angleChild pEmb cEmb
-                                a2 = angleParent pEmb cEmb alpha
+                            let a1 = angleChild alpha pEmb cEmb
+                                a2 = angleParent alpha pEmb cEmb
                                 score = a1 - a2
-                            in if score > threshold then 1.0 else 0.0
+                            -- in if score > threshold then 1.0 else 0.0
+                            in score
                         _ -> notFoundValue
                 _ -> notFoundValue
 
@@ -84,7 +79,7 @@ neuralDTSBuilderNoPickle = do
 
 main :: IO ()
 main = do
-    oracle <- neuralDTSBuilderNoPickle
+    oracle <- neuralDTSBuilder
     print $ oracle "有袋動物" "経済"
     print $ oracle "有袋動物" "カンガルー"
     print $ oracle "有袋動物" "aaa" -- vocabにない単語なので-1.0が返るはず
---
+++ Haskell/app/NeuralDTSBuilderTest.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module NeuralDTSBuilderTest where
+
+import qualified Data.Map.Strict as M
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Data.Text.Read as TR
+import Data.Maybe (mapMaybe)
+import NeuralDTSBuilderUtils (angleChild, angleParent)
+
+-- word,dim1,dim2,dim3,... 形式でロードする
+loadWordEmbeddings :: FilePath -> IO (M.Map String [Float])
+loadWordEmbeddings path = do
+    contents <- TIO.readFile path
+    let ls = drop 1 $ T.lines contents
+
+    let parseFloat t =
+            case TR.double t of
+              Right (d, _) -> Just (realToFrac d :: Float)
+              _ -> Nothing
+
+    let parseLine line =
+            case T.splitOn "," line of
+              (wordTxt : dimsTxt) ->
+                  let dims = mapMaybe parseFloat dimsTxt
+                  in Just (T.unpack wordTxt, dims)
+              _ -> Nothing
+
+    pure $ M.fromList (mapMaybe parseLine ls)
+
+
+-- Embedding（word→embedding）でOracleの実装
+neuralDTSBuilder :: IO (String -> String -> Float)
+neuralDTSBuilder = do
+    let embPath   = "data/NeuralDTSBuilderTest.csv"
+    let alpha     = 0.1
+    let threshold = 0.5
+    let notFoundValue = -1.0 :: Float
+
+    embMap <- loadWordEmbeddings embPath
+
+    -- oracl :: (DTTdB.ConName -> DTTdB.ConName -> Float) にしたい (DTTdB.ConNameの呼び出し方わからない)
+    let oracle :: String -> String -> Float
+        oracle parent child =
+            case (M.lookup parent embMap, M.lookup child embMap) of
+                (Just pEmb, Just cEmb) ->
+                    let a1 = angleChild alpha pEmb cEmb
+                        a2 = angleParent alpha pEmb cEmb
+                        score = a1 - a2
+                    -- in if score > threshold then 1.0 else 0.0
+                    in score
+                _ -> notFoundValue
+
+    pure oracle
+
+
+main :: IO ()
+main = do
+    oracle <- neuralDTSBuilder
+    print $ oracle "有袋動物" "経済"
+    print $ oracle "有袋動物" "カンガルー"
+    print $ oracle "有袋動物" "aaa"
+    print $ oracle "艦隊" "アルカリ土類金属"
